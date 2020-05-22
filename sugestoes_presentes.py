65eab54a1d983bd9fd2ccf05fe60e842cc1b9cd6
Letícia Lima <lelima039@gmail.com>
2020-05-22 17:13:05 -0300

    Add files via upload

---
+++ sugestoes_presentes.py
@@ -0,0 +1,44 @@
+import opcoes_presentes
+import pontuacao_categorias
+import random
+
+pontuacao_presentes = {pontuacao_categorias.cont_cultura: opcoes_presentes.presentes_cultura,
+                       pontuacao_categorias.cont_esporte: opcoes_presentes.presentes_esportes,
+                       pontuacao_categorias.cont_entretenimento: opcoes_presentes.presentes_entretenimento,
+                       pontuacao_categorias.cont_conexao_social: opcoes_presentes.presentes_conexao_social,
+                       pontuacao_categorias.cont_hobby: opcoes_presentes.presentes_hobby,
+                       pontuacao_categorias.cont_vestuario: opcoes_presentes.presentes_vestuario,
+                       pontuacao_categorias.cont_saude: opcoes_presentes.presentes_saude,
+                       pontuacao_categorias.cont_gastronomia: opcoes_presentes.presentes_gastronomia}
+
+pontuacao_cres = sorted(pontuacao_presentes)
+sugestoes_finais = []
+
+while len(sugestoes_finais) != 9:
+    presentes_categoria1 = random.choice(pontuacao_presentes[pontuacao_cres[-1]])
+    if presentes_categoria1 not in sugestoes_finais:
+        sugestoes_finais.append(presentes_categoria1)
+    if presentes_categoria1 in sugestoes_finais:
+        presentes_categoria1 = random.choice(pontuacao_presentes[pontuacao_cres[-1]])
+
+    presentes_categoria2 = random.choice(pontuacao_presentes[pontuacao_cres[-2]])
+    if presentes_categoria2 not in sugestoes_finais:
+        sugestoes_finais.append(presentes_categoria2)
+    if presentes_categoria2 in sugestoes_finais:
+        presentes_categoria2 = random.choice(pontuacao_presentes[pontuacao_cres[-2]])
+
+    presentes_categoria3 = random.choice(pontuacao_presentes[pontuacao_cres[-3]])
+    if presentes_categoria3 not in sugestoes_finais:
+        sugestoes_finais.append(presentes_categoria3)
+    if presentes_categoria3 in sugestoes_finais:
+        presentes_categoria3 = random.choice(pontuacao_presentes[pontuacao_cres[-3]])
+
+    if len(sugestoes_finais) == 9:
+        break
+
+print('Oi {}! Os presentes que {} vai amar são: \n'.format(pontuacao_categorias.tabela['3'][5],
+                                                           pontuacao_categorias.tabela['4'][5]))
+cont = 0
+for i in sugestoes_finais:
+    cont += 1
+    print('{}. {}'.format(cont, i))
